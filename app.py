81abcb3c4e9d41eb3db5a5c272faa5b0836f7306
Kevin Stradtman <stradtkt22@gmail.com>
2019-01-26 12:58:01 -0500

    ran the program a got back a database file without errors

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from peewee import *
-
+import datetime
 db = SqliteDatabase("diary.db")
 
-class Entry():
+class Entry(Model):
+    content = TextField()
+    timestamp = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         database = db
 
+def initialize():
+    """Create the db and table if they do not exist"""
+    db.connect()
+    db.create_tables([Entry], safe=True)
 
 def menu_loop():
     """Show the menu"""
@@ -25,4 +31,5 @@ def delete_entry():
 
 
 if __name__ == "__main__":
+    initialize()
     menu_loop()
