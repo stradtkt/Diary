750ceec8bd260ef5a732562eb190e33782f1b93c
Kevin Stradtman <stradtkt22@gmail.com>
2019-01-26 12:47:32 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from peewee import *
+
+db = SqliteDatabase("diary.db")
+
+class Entry():
+
+    class Meta:
+        database = db
+
+
+def menu_loop():
+    """Show the menu"""
+
+
+def add_entry():
+    """Add an entry"""
+
+
+def view_entries():
+    """View previous entries"""
+
+
+def delete_entry():
+    """Delete an entry"""
+
+
+if __name__ == "__main__":
+    menu_loop()
